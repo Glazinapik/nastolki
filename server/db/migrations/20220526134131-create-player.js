1e29e0678081520c47dcae290e797b74b089b102
Aleksandr Schepilov <gornostay4eg@mail.ru>
2022-05-26 17:14:21 +0300

    autorization/registration

---
+++ server/db/migrations/20220526134131-create-player.js
@@ -0,0 +1,37 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Players', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      meeting_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Meetings', // tableName
+          key: 'id',
+        },
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users', // tableName
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Players');
+  },
+};
